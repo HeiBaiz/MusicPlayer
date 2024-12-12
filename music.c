f4c208a7f8cc78390268e7f7d3179aa2c1912aa3
baini <1485890854@qq.com>
2024-12-12 17:08:18 +0800

    完成

--- music.c
+++ music.c
@@ -1,10 +1,18 @@
 #include "music.h"
 
-#include <sys/types.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-char path1[]="/usr/bin/madplay",path2[]="./.mp3";
+char path1[]="/usr/bin/madplay",path2[]="./1.mp3";
+int j=1;
+int musicFlag=0,temp;
+
+#define musicMin 0
+#define musicMax 3
 
 int musicPlayer(pid_t *pid,int i){
     // pid_t pid = -2;
@@ -19,11 +27,50 @@ int musicPlayer(pid_t *pid,int i){
                     return -1;
                 } else if(pid==0) {
                     int res = execl(path1,"madpaly",path2,NULL);
+                    if (res == -1)
+                    {
+                        perror("execl error");
+                        return -1;
+                    }
                 }
             }
         case 2:
             // 上一曲
-            kill()
+            kill(pid,SIGTERM);
+            if(j-- == musicMin){
+                j = musicMax;
+            }
+            sprintf(path2,"./%d.mp3",j);
+            int res = execl(path1,"madpaly",path2,NULL);
+            if (res == -1)
+            {
+                perror("execl error");
+                return -1;
+            }
+        case 3:
+            // 下一曲
+            kill(pid,SIGTERM);
+            if(j++ == musicMax){
+                j = musicMin;
+            }
+            sprintf(path2,"./%d.mp3",j);
+            int res = execl(path1,"madpaly",path2,NULL);
+            if (res == -1)
+            {
+                perror("execl error");
+                return -1;
+            }
+        case 4:
+            // 暂停
+            if(!musicFlag){
+                kill(pid,SIGSTOP);
+                musicFlag=1;
+            }else {
+                kill(pid,SIGCONT);
+                musicFlag-0;
+            }
+            
+
     }
     execl()
 }
